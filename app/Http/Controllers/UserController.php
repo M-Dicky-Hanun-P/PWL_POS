b8f41f79cb41878b040a082f9f7b2ef739f672d9
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-09-18 14:39:44 +0700

    Upload Jobsheet 4_Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,10 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::find(1);
+        //Praktikum 2
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
 
     }
